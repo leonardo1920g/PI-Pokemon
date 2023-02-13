49a0658fe759e0d977702aab70d614c1b61a3953
leonardo guerrero <leo.19-20@hotmail.com>
2023-02-13 01:44:29 -0500

    commit2.1

--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -2,21 +2,20 @@ const { Pokemon, Type } = require("../db");
 const axios = require('axios');
 
 const cleanArray = (arr) =>
-    arr.map((info) => {
-   
+    arr.map((info) => {   
         return {
-                    id: info.data.id,
-                    name: info.data.name,
-                    hp: info.data.stats[0].base_stat,
-                    attack: info.data.stats[1].base_stat,
-                    defense: info.data.stats[2].base_stat,
-                    speed: info.data.stats[5].base_stat,
-                    height: info.data.height,
-                    weight: info.data.weight,
-                    types: info.data.types.map((t) => t.type.name),
-                    image: info.data.sprites.other["official-artwork"]['front_default'],
-                    createdInDb: false,
-                };
+            id: info.data.id,
+            name: info.data.name,
+            hp: info.data.stats[0].base_stat,
+            attack: info.data.stats[1].base_stat,
+            defense: info.data.stats[2].base_stat,
+            speed: info.data.stats[5].base_stat,
+            height: info.data.height,
+            weight: info.data.weight,
+            types: info.data.types.map((t) => t.type.name),
+            image: info.data.sprites.other["official-artwork"]['front_default'],
+            createdInDb: false,
+        };
 });
 
 const cleanArrayid = (pokemon) => {
@@ -43,19 +42,11 @@ const createPokemon = async (
 const getPokemonById = async (id, source) => {
 
     const pokemonRaw = source === "api" 
-        ? (await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`))
-        .data
+        ? (await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)).data
         : await Pokemon.findByPk(id, {
-            include: {
-                model: Type,
-                attributes: ["name"],
-            },
-        });
-        
-        
-       
+            include: { model: Type, attributes: ["name"] },
+        });     
     const pokemon = cleanArrayid(pokemonRaw);
-
     return pokemon;
 };
 
@@ -63,51 +54,84 @@ const getAllPokemons = async () => {
 
     const databasePokemons = await Pokemon.findAll();
 
-    const api = await axios.get('https://pokeapi.co/api/v2/pokemon')
-    const response = api.data.results?.map(elemento => axios.get(elemento.url))
-    const responseApi = await axios.all(response)
-    const apiPokemons = cleanArray(responseApi)
+    const api = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const response = api.data.results?.map(elemento => axios.get(elemento.url));
+    const responseApi = await axios.all(response);
+    const apiPokemons = cleanArray(responseApi);
     
-    // .map((info) => {
-    //     return {
-    //         id: info.data.id,
-    //         name: info.data.name,
-    //         hp: info.data.stats[0].base_stat,
-    //         attack: info.data.stats[1].base_stat,
-    //         defense: info.data.stats[2].base_stat,
-    //         speed: info.data.stats[5].base_stat,
-    //         height: info.data.height,
-    //         weight: info.data.weight,
-    //         types: info.data.types.map((t) => t.type.name),
-    //         image: info.data.sprites.other["official-artwork"]['front_default'],
-    //         createdInDb: false,
-    //     }
-    //  });
-
-    return [...databasePokemons, ...apiPokemons]
+    return [...databasePokemons, ...apiPokemons];
 
 };
 
 const searchPokemonByName = async (name) => {
+    const Pokemons = await getAllPokemons();
+    const pokemonName = Pokemons.filter(e => e.name === name.trim().toLowerCase());
+    return pokemonName.length ? pokemonName[0] : "THE POKEMON DOES NOT EXIST";
+};
+ 
+module.exports = { 
+    createPokemon, 
+    getPokemonById,
+    getAllPokemons,
+    searchPokemonByName
+};
 
-    const databasePokemon = await Pokemon.findAll({where: {name: name.trim().toLowerCase()}});
+    
 
-    let pokemonByNameAPI = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
-    let queryFromAPI =  {
-          id: pokemonByNameAPI.data.id,
-          name: pokemonByNameAPI.data.name,
-          hp: pokemonByNameAPI.data.stats[0].base_stat,
-          attack: pokemonByNameAPI.data.stats[1].base_stat,
-          defense: pokemonByNameAPI.data.stats[2].base_stat,
-          speed: pokemonByNameAPI.data.stats[5].base_stat,
-          height: pokemonByNameAPI.data.height,
-          weight: pokemonByNameAPI.data.weight,
-          types: pokemonByNameAPI.data.types.map((t) => t.type.name),
-          image: pokemonByNameAPI.data.sprites.other["official-artwork"]['front_default'],
-          createdInDb: false,
-      }
+    //const databasePokemon = await Pokemon.findAll({where: {name: name.trim().toLowerCase()}});
 
-    // const apiPokeUrl = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    // let pokemonRaw = (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)) //${name.toLowerCase()}
+    // .data
+    
+    //const pokemon = cleanArrayid(pokemonRaw);
+
+    //console.log(pokemonRaw);
+
+    // const pokemonsRequest = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
+    // //me devuelve los pokemons traidos con un name y una url de cada pokemon
+    // const pokemonsSubrequest = pokemonsRequest.data.results.map(obj => axios.get(obj.url));
+    // //hago el axios pero a la sub url TERMINAR DE VER COMO FUNCIONA EL data.results.map
+    // const infoUrlPokemons = await axios.all(pokemonsSubrequest);
+    // //llama a todas las sub url, solicitudes simultaneas 
+    // let pokemons = infoUrlPokemons.map(obj => obj.data);
+    // //obtengo la data de cada pokemon por su suburl
+    // let informacionPokemons = pokemons.map(pokemon => objPokeApi(pokemon))
+    // //return informacionPokemons
+    
+    // then(() => {
+    //     console.log("funciona");
+    // })
+
+
+
+    // let queryFromAPI =  {
+    //       id: pokemonByNameAPI.data.id,
+    //       name: pokemonByNameAPI.data.name,
+    //       hp: pokemonByNameAPI.data.stats[0].base_stat,
+    //       attack: pokemonByNameAPI.data.stats[1].base_stat,
+    //       defense: pokemonByNameAPI.data.stats[2].base_stat,
+    //       speed: pokemonByNameAPI.data.stats[5].base_stat,
+    //       height: pokemonByNameAPI.data.height,
+    //       weight: pokemonByNameAPI.data.weight,
+    //       types: pokemonByNameAPI.data.types.map((t) => t.type.name),
+    //       image: pokemonByNameAPI.data.sprites.other["official-artwork"]['front_default'],
+    //       createdInDb: false,
+    //   }
+
+    
+
+    //return [...databasePokemon, ...informacionPokemons]
+
+//};
+
+
+
+
+
+
+
+
+// const apiPokeUrl = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
     // const results = apiPokeUrl.data;
 
     // const pokemonInfo = {
@@ -127,14 +151,3 @@ const searchPokemonByName = async (name) => {
     //     .data.results;
         
     //     const pokemon = cleanArrayid(apiPokemonRaw);
-
-    return [...databasePokemon, ...queryFromAPI]
-
-};
-
-module.exports = { 
-    createPokemon, 
-    getPokemonById,
-    getAllPokemons,
-    searchPokemonByName
-};
--- api/src/handlers/pokemonHandler.js
+++ api/src/handlers/pokemonHandler.js
@@ -3,26 +3,23 @@ const {
     getPokemonById,
     getAllPokemons,
     searchPokemonByName
-} = require("../controllers/pokemonController")
+} = require("../controllers/pokemonController");
  
 const getPokemonsHandler = async    (req, res) => {
     const { name } = req.query;
 
     try {
     const results = name ? await searchPokemonByName(name) : await getAllPokemons();
-
-    res.status(200).json(results);
-
+        res.status(200).json(results);
     } catch (error) {
-        res.status(404).send("THE POKEMON DOES NOT EXIST")
-
-    }
-    
+        res.status(404).send({ error: error.message });
+    };    
  };
 
 const getPokemonHandler = async (req, res) => {
     const { id } = req.params;
     const source = isNaN(id)? "bdd" : "api";
+
     try{
         const pokemon = await getPokemonById(id, source);
         res.status(200).json(pokemon);
@@ -36,8 +33,7 @@ const createPokemonHandler = async (req, res) => {
     
     try {
         const newPokemon = await createPokemon(name, image, hp, attack, defense, speed, height, weight);
-        res.status(200).json({data: newPokemon, message: "POKEMON CREATED SUCCESSFULLY"}) 
-
+        res.status(200).json({data: newPokemon, message: "POKEMON CREATED SUCCESSFULLY"});
     } catch (error) {
         res.status(400).json({ error: error.message });
     };
@@ -46,5 +42,5 @@ const createPokemonHandler = async (req, res) => {
 module.exports = {
     getPokemonsHandler,
     getPokemonHandler,
-    createPokemonHandler,
+    createPokemonHandler
 };
