3f74028d6a0890ee72f7ac58788c24e75aa1445b
Leonardo Guerrero <leo.19-20@hotmail.com>
2023-02-15 15:13:44 -0500

    tree

--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -2,19 +2,19 @@ const { Pokemon, Type } = require("../db");
 const axios = require('axios');
 
 const cleanArray = (arr) =>
-    arr.map((info) => {   
+    arr.map((Data) => {   
         return {
-            id: info.data.id,
-            name: info.data.name,
-            hp: info.data.stats[0].base_stat,
-            attack: info.data.stats[1].base_stat,
-            defense: info.data.stats[2].base_stat,
-            speed: info.data.stats[5].base_stat,
-            height: info.data.height,
-            weight: info.data.weight,
-            types: info.data.types.map((t) => t.type.name),
-            image: info.data.sprites.other["official-artwork"]['front_default'],
-            createdInDb: false,
+            id: Data.data.id,
+            name: Data.data.name,
+            hp: Data.data.stats[0].base_stat,
+            attack: Data.data.stats[1].base_stat,
+            defense: Data.data.stats[2].base_stat,
+            speed: Data.data.stats[5].base_stat,
+            height: Data.data.height,
+            weight: Data.data.weight,
+            types: Data.data.types.map((t) => t.type.name),
+            image: Data.data.sprites.other["official-artwork"]['front_default'],
+            created: false,
         };
 });
 
@@ -60,13 +60,28 @@ const getAllPokemons = async () => {
     const apiPokemons = cleanArray(responseApi);
     
     return [...databasePokemons, ...apiPokemons];
-
 };
 
 const searchPokemonByName = async (name) => {
-    const Pokemons = await getAllPokemons();
-    const pokemonName = Pokemons.filter(e => e.name === name.trim().toLowerCase());
-    return pokemonName.length ? pokemonName[0] : "THE POKEMON DOES NOT EXIST";
+
+    const nameSearch = name.trim().toLowerCase();
+    const databasePokemon = await Pokemon.findAll({where: {name: nameSearch}});
+
+    const apiPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${nameSearch}`)
+        .then (({data}) =>({
+            id: data.id,
+            name: data.name,
+            hp: data.stats[0].base_stat,
+            attack: data.stats[1].base_stat,
+            defense: data.stats[2].base_stat,
+            speed: data.stats[5].base_stat,
+            height: data.height,
+            weight: data.weight,
+            types: data.types.map((t) => t.type.name),
+            image: data.sprites.other["official-artwork"]['front_default'],
+            created: false,
+        }));
+    return [...databasePokemon, apiPokemon];
 };
  
 module.exports = { 
@@ -76,78 +91,8 @@ module.exports = {
     searchPokemonByName
 };
 
-    
-
-    //const databasePokemon = await Pokemon.findAll({where: {name: name.trim().toLowerCase()}});
-
-    // let pokemonRaw = (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)) //${name.toLowerCase()}
-    // .data
-    
-    //const pokemon = cleanArrayid(pokemonRaw);
-
-    //console.log(pokemonRaw);
-
-    // const pokemonsRequest = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
-    // //me devuelve los pokemons traidos con un name y una url de cada pokemon
-    // const pokemonsSubrequest = pokemonsRequest.data.results.map(obj => axios.get(obj.url));
-    // //hago el axios pero a la sub url TERMINAR DE VER COMO FUNCIONA EL data.results.map
-    // const infoUrlPokemons = await axios.all(pokemonsSubrequest);
-    // //llama a todas las sub url, solicitudes simultaneas 
-    // let pokemons = infoUrlPokemons.map(obj => obj.data);
-    // //obtengo la data de cada pokemon por su suburl
-    // let informacionPokemons = pokemons.map(pokemon => objPokeApi(pokemon))
-    // //return informacionPokemons
-    
-    // then(() => {
-    //     console.log("funciona");
-    // })
-
-
-
-    // let queryFromAPI =  {
-    //       id: pokemonByNameAPI.data.id,
-    //       name: pokemonByNameAPI.data.name,
-    //       hp: pokemonByNameAPI.data.stats[0].base_stat,
-    //       attack: pokemonByNameAPI.data.stats[1].base_stat,
-    //       defense: pokemonByNameAPI.data.stats[2].base_stat,
-    //       speed: pokemonByNameAPI.data.stats[5].base_stat,
-    //       height: pokemonByNameAPI.data.height,
-    //       weight: pokemonByNameAPI.data.weight,
-    //       types: pokemonByNameAPI.data.types.map((t) => t.type.name),
-    //       image: pokemonByNameAPI.data.sprites.other["official-artwork"]['front_default'],
-    //       createdInDb: false,
-    //   }
-
-    
-
-    //return [...databasePokemon, ...informacionPokemons]
-
-//};
-
-
-
-
-
-
-
-
-// const apiPokeUrl = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    // const results = apiPokeUrl.data;
-
-    // const pokemonInfo = {
-    //   id: results.id,
-    //   name: results.name,
-    //   hp: results.data.stats[0].base_stat,
-    //   attack: results.data.stats[1].base_stat,
-    //   defense: results.data.stats[2].base_stat,
-    //   speed: results.data.stats[5].base_stat,
-    //   height: results.height,
-    //   weight: results.weight,
-    //   types: results.types.map((t) => t.type.name),
-    //   img: results.sprites.other["official-artwork"].front_default,
-    //   create: false,
-    // };  
-    // const apiPokemonRaw = (await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`))
-    //     .data.results;
-        
-    //     const pokemon = cleanArrayid(apiPokemonRaw);
+//const searchPokemonByName = async (name) => {
+//     const Pokemons = await getAllPokemons();
+//     const pokemonName = Pokemons.filter(e => e.name === name.trim().toLowerCase());
+//     return pokemonName.length ? pokemonName[0] : "THE POKEMON DOES NOT EXIST";
+// };
