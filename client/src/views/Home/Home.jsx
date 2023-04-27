f09bc738b716db820386e70ee3abb22402bbd02a
Leonardo Guerrero <leo.19-20@hotmail.com>
2023-04-27 01:44:48 -0500

    home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import { useDispatch } from "react-redux";
-import { getPokemons, getTypes, } from "../../Redux/actions";
+import { clearPokemons, getPokemons, getTypes, } from "../../Redux/actions";
 import styles from "./Home.module.css";
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
     const dispatch = useDispatch();
         
     useEffect(() => {   
-        dispatch(clearPokemons());     
+        dispatch(clearPokemons());
         dispatch(getPokemons());
         dispatch(getTypes());        
     },[dispatch]); 
